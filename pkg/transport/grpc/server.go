f2f1c2c16085037e4cb2193ba22c7695b1cbafad
Andrey <andrey.burtasov@yandex.ru>
2023-08-17 19:28:18 +0300

    added project

---
+++ pkg/transport/grpc/server.go
@@ -0,0 +1,49 @@
+package transport
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aburtasov/books"
+	"github.com/aburtasov/books/api"
+	"github.com/aburtasov/books/pkg/service"
+)
+
+type GRPCServer struct {
+	services service.Service
+	api.UnsafeBookerServer
+}
+
+func (s *GRPCServer) GetBooks(ctx context.Context, author *api.Author) (*api.Books, error) {
+
+	var input books.Author
+	input.FirstName = author.FirstName
+	input.SecondName = author.SecondName
+
+	if input.SecondName == "" {
+		return &api.Books{}, errors.New("empty second name")
+	}
+
+	books, err := s.services.Book.GetBooks(input)
+	if err != nil {
+		return &api.Books{}, errors.New("internal server error")
+	}
+
+	return &api.Books{Title: books}, nil
+
+}
+
+func (s *GRPCServer) GetAuthor(ctx context.Context, books *api.Books) (*api.Author, error) {
+	return &api.Author{}, nil
+}
+func (s *GRPCServer) AddBook(ctx context.Context, books *api.Book) (*api.ResponceID, error) {
+	return &api.ResponceID{}, nil
+}
+func (s *GRPCServer) AddAuthor(ctx context.Context, author *api.Author) (*api.ResponceID, error) {
+	return &api.ResponceID{}, nil
+}
+
+func (s *GRPCServer) mustEmbedUnimplementedBookerServer() {
+	fmt.Printf("s.UnsafeBookerServer: %v\n", s.UnsafeBookerServer)
+}
